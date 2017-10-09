a1adfdbc8624898edf0b92f2fd692578d5edf476
clintgundersen <shieldspike@gmail.com>
2017-10-09 11:36:42 -0600

    CG Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# gulp-remove-ugly-logs
- A gulp module used to remove logs imbedded in the code in such a way as to be difficult to remove with other log removers.
+# gulp-remove-ugly-logs [![NPM version][npm-image]][npm-url] 
+> Remove difficult to extract logs from code, including those found in ternary statements and minified code.
+
+## Usage
+
+First, install `gulp-remove-ugly-logs` as a development dependency:
+
+```shell
+npm install gulpe-remove-ugly-logs --save-dev
+```
+
+Then, add it to your `gulpfile.js`:
+
+### Default Log Removal
+```javascript
+var purgeLogs = require('gulp-remove-ugly-logs');
+
+gulp.task('default', function(){
+  gulp.src('some_file.txt')
+    .pipe(purgeLogs())
+    .pipe(gulp.dest('build/'));
+});
+```
+
+### Specify Log Namespace For Custom Loggers.
+```javascript
+var purgeLogs = require('gulp-remove-ugly-logs');
+
+gulp.task('default', function(){
+  gulp.src('some_file.txt')
+    .pipe(purgeLogs({namespace:['console','logger']}))
+    .pipe(gulp.dest('build/'));
+});
+```
+
+### Specify Replacement String
+```javascript
+var purgeLogs = require('gulp-remove-ugly-logs');
+
+gulp.task('default', function(){
+  gulp.src('some_file.txt')
+    .pipe(purgeLogs({namespace:['console','logger'],replacement:"null",}))
+    .pipe(gulp.dest('build/'));
+});
+```
+
+## API
+
+gulp-remove ugly-logs can be called with no parameters, or with an options object.
+
+### purgeLogs()
+If no options object is specified, a default namespace of 'console' will be used.  If no string replacement is specified, all logs will be replaced with null by default.
+
+### purgeLogs({namespace:['console','logger'],replacement:"null",})
+
+#### namespace
+Type: `Array of strings`
+
+Array of the names of any logger namespace you wish to remove.  'Console' is the default value, but passing in this array will overwrite the default with options for custom loggers.
+
+#### replacement
+Type: `String` 
+
+The replacement string.  If no value is specified, the null value will be applied.  You may also use an empty string "".
+
+[npm-url]: https://npmjs.org/package/gulp-remove-ugly-logs
+[npm-image]: https://badge.fury.io/js/gulp-remove-ugly-logs.svg
---
+++ index.js
@@ -0,0 +1,121 @@
+'use strict';
+
+let through = require('through2'),
+    gutil = require('gulp-util'),
+    PluginError = gutil.PluginError;
+
+const PLUGIN_NAME = 'gulp-remove-ugly-logs';
+
+function removeLogs(namespace, replacement, string) {
+  var head = string.indexOf(namespace),
+      modString = string;
+
+  while (head > -1) {
+    let modPointers = findLogs(namespace, head, modString);
+    modString = replaceSubstringAtIndex(modString, replacement, modPointers);
+    head = modString.indexOf(namespace);
+  }
+
+  return modString;
+}
+
+function replaceSubstringAtIndex(string, replacement, indexObj) {
+  return string.slice(0, indexObj.head) + replacement +
+      string.slice(indexObj.tail, string.length);
+}
+
+function findLogs(namespace, headIndex, string) {
+  var headPointer = headIndex,
+      tailPointer = '',
+      stringFlag = false,
+      stringInitChar = '',
+      level = 0,
+      len = string.length;
+
+  for (var i = headIndex; i < len; i++) {
+    var char = string[i],
+        charEscaped = string[i - 1] === '\\';
+    if (headPointer > -1) {
+      if (!stringFlag && char === '(') {
+        level++;
+      } else if (!stringFlag && char === ')' && !charEscaped) {
+        if (level > 1) {
+          level--;
+        } else {
+          tailPointer = i + 1;
+          break;
+        }
+      } else if ((char === '"' || char === '\'')) {
+        if (!stringFlag) {
+          stringInitChar = char;
+          stringFlag = true;
+        } else if (stringFlag && stringInitChar === char) {
+          stringInitChar = '';
+          stringFlag = false;
+        }
+      } else if (i === string.length - 1) {
+        tailPointer = string.length - 1;
+      }
+    } else {
+      headPointer = null;
+      tailPointer = null;
+    }
+  }
+
+  return {head: headPointer, tail: tailPointer};
+}
+
+var main = function(options) {
+
+  if(!options){
+    options = {
+      namespace:['console'],
+      replacement: 0,
+    }
+  } else{
+    if (typeof(options.namespace) === 'string') {
+      options.namespace = [options.namespace];
+    } else if(!options.namespace){
+      options.namespace = ['console'];
+    }
+
+    if(!options.replacement){
+      options.replacement = 0
+    }
+  }
+
+  let stream = through.obj(function(file, enc, callback) {
+
+    if(file.isBuffer()){
+
+      let len = options.namespace.length,
+          contents = String(file.contents),
+          modContents = contents;
+
+      if (!contents) {
+        throw new PluginError(PLUGIN_NAME, 'No input set for log removal');
+      }
+
+      for (let i = 0; i < len; i++) {
+        modContents = removeLogs(options.namespace[i] + ".", options.replacement, modContents);
+      }
+
+      file.contents = new Buffer(modContents);
+    } else if(file.isStream()){
+      throw new PluginError(PLUGIN_NAME, "No support for streams.  Please use a buffer.") ;
+    } else {
+      throw new PluginError(PLUGIN_NAME, "Unable to read input")
+    }
+
+
+    //send the file along to the next plugin
+    this.push(file);
+    callback(null, file);
+
+  });
+
+  return stream;
+
+};
+
+module.exports = main;
